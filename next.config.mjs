b8ecae3faa59502d5a3e2bf7001bcb599dfeee0b
SOURAV SINGHAL <souravsinghal2004@gmail.com>
2025-07-13 18:22:31 +0530

    Fixed Netlify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-import withNetlify from '@netlify/next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["your-image-domain.com"], // Optional: if you're loading remote images
-  },
 };
 
-export default withNetlify(nextConfig);
+export default nextConfig;
